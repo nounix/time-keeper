64e6c806a2e25d48f8523a7587299e0a1bef706a
nounix <martin.muenning@gmail.com>
2023-04-07 15:55:38 +0200

    refactor and add sum of tracked days

--- main.py
+++ main.py
@@ -1,59 +1,63 @@
 import time
 from datetime import datetime, timedelta
 
-
 # disable default TTY login prompt for not grabbing stdio
 # $ sudo systemctl disable getty@tty1.service
 
 TIMES_DIR = "/home/pi/times/"
 
+def cleanUpFile(f, lines):
+    f.seek(0)
+    f.writelines(lines)
+    f.truncate()
 
-def calcSumTime(user_id):
-    summe = 0
-    last_month = (datetime.today().replace(day=1) -
-                  timedelta(days=1)).strftime("%Y-%m")
+def readLastLine(userId):
+    try:
+        with open(TIMES_DIR + datetime.now().strftime("%Y-%m") + "-" + userId + ".tk", "r+") as f:
+            lines = [line for line in f if line.startswith(("S", "E"))]
+            cleanUpFile(f, lines)
+            return lines[-1]
+    except FileNotFoundError as e:
+        print(f"File not found for user: {userId}\n\tFileNotFoundError in updateTime(): {e}")
+    except IndexError as e:
+        print(f"File empty for user: {userId}\n\tIndexError in updateTime(): {e}")
+
+def calcSumOfTrackedDaysAndMinutes(userId):
+    daysTracked = set([])
+    sumOfMinutesTracked = 0
+    lastMonth = (datetime.today().replace(day=1) - timedelta(days=1)).strftime("%Y-%m")
 
     try:
-        with open(TIMES_DIR + last_month + "-" + user_id + ".tk", "r") as f:
+        with open(TIMES_DIR + lastMonth + "-" + userId + ".tk", "r") as f:
             lines = f.read().splitlines()
 
             for line in lines:
-                if(len(line.split('|')) > 3 and line.split('|')[3].isnumeric()):
-                    summe += int(line.split('|')[3])
+                if(len(line.split('|')) > 3):
+                    daysTracked.add(line.split('|')[1])
+                    if(line.split('|')[3].isnumeric()):
+                        sumOfMinutesTracked += int(line.split('|')[3])
 
-        with open(TIMES_DIR + last_month + "-" + user_id + ".tk", "a+") as f:
-            f.write("SUM|{0}\n".format(summe))
-    except Exception as e:
-        print("Calc sum failed for:", user_id, "\n\tException:", e)
+        with open(TIMES_DIR + lastMonth + "-" + userId + ".tk", "a+") as f:
+            f.write(f"SUM_DAYS|{len(daysTracked)}\nSUM_MINUTES|{sumOfMinutesTracked}\n")
+    except FileNotFoundError as e:
+        print(f"File not found for user: {userId}\n\tFileNotFoundError in calcSumOfTrackedDaysAndMinutes(): {e}")
 
+def updateTime(userId):
+    lastLine = readLastLine(userId)
 
-def update_time(user_id):
-    last_line = ""
-    last_date = datetime.now()
+    if not lastLine:
+        calcSumOfTrackedDaysAndMinutes(userId)
 
-    try:
-        with open(TIMES_DIR + datetime.now().strftime("%Y-%m") + "-" + user_id + ".tk", "r+") as f:
-            lines = [line for line in f if line.startswith(("S", "E"))]
-            f.seek(0)
-            f.writelines(lines)
-            f.truncate()
+    lastDate = datetime.now() if not lastLine else datetime.strptime(' '.join(lastLine.split('|')[1:3]).rstrip(), '%Y-%m-%d %H:%M:%S')
 
-            last_line = lines[-1]
-
-            last_date = datetime.strptime(
-                ' '.join(last_line.split('|')[1:3]).rstrip(), '%Y-%m-%d %H:%M:%S')
-    except Exception as e:
-        print("Update time failed for:", user_id, "\n\tException:", e)
-        calcSumTime(user_id)
-
-    with open(TIMES_DIR + datetime.now().strftime("%Y-%m") + "-" + user_id + ".tk", "a+") as f:
-        if last_line == "" or last_line[0] == 'E':
-            if last_date.date() == datetime.now().date():
-                if (datetime.now() - last_date).seconds > 3600:
+    with open(TIMES_DIR + datetime.now().strftime("%Y-%m") + "-" + userId + ".tk", "a+") as f:
+        if not lastLine or lastLine[0] == 'E':
+            if lastDate.date() == datetime.now().date():
+                if (datetime.now() - lastDate).seconds > 3600:
                     f.write("S|{0}|W\n".format(
-                        (last_date + timedelta(minutes=60)).strftime("%Y-%m-%d|%H:%M:%S")))
+                        (lastDate + timedelta(minutes=60)).strftime("%Y-%m-%d|%H:%M:%S")))
                     f.write("E|{0}|{1}\n".format(datetime.now().strftime("%Y-%m-%d|%H:%M:%S"), int(
-                        (datetime.now() - (last_date + timedelta(minutes=60))).seconds / 60)))
+                        (datetime.now() - (lastDate + timedelta(minutes=60))).seconds / 60)))
                 else:
                     f.write("S|{0}\n".format(
                         datetime.now().strftime("%Y-%m-%d|%H:%M:%S")))
@@ -61,33 +65,31 @@ def update_time(user_id):
                 f.write("S|{0}\n".format(
                     datetime.now().strftime("%Y-%m-%d|%H:%M:%S")))
         else:
-            if last_date.date() != datetime.now().date():
+            if lastDate.date() != datetime.now().date():
                 f.write("E|{0}|{1}|W\n".format(
-                    (last_date + timedelta(minutes=10)).strftime("%Y-%m-%d|%H:%M:%S"), 10))
+                    (lastDate + timedelta(minutes=10)).strftime("%Y-%m-%d|%H:%M:%S"), 10))
                 f.write("S|{0}\n".format(
                     datetime.now().strftime("%Y-%m-%d|%H:%M:%S")))
             else:
                 f.write("E|{0}|{1}\n".format(datetime.now().strftime(
-                    "%Y-%m-%d|%H:%M:%S"), int((datetime.now() - last_date).seconds / 60)))
-
+                    "%Y-%m-%d|%H:%M:%S"), int((datetime.now() - lastDate).seconds / 60)))
 
 def test():
     global TIMES_DIR
     TIMES_DIR = "/tmp/"
 
     while True:
-        update_time("1337")
+        updateTime("1337")
         time.sleep(3)
-
 
 def main():
     fp = open('/dev/tty0', 'rb')
 
     while True:
-        buffer = fp.readline().rstrip().decode('UTF-8')
-        update_time(buffer)
+        userId = fp.readline().rstrip().decode('UTF-8')
+        updateTime(userId)
         time.sleep(1)
 
 
 if __name__ == "__main__":
-    main()
+    test()
